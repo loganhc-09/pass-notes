cd0a6c266b93c15b6d81c7fa18345100e70b23f2
ajpether <35989837+ajpether@users.noreply.github.com>
2026-01-29 21:06:11 -0500

    Fix: remove router, render Quiz directly for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
 import Quiz from './Quiz.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/quiz" element={<Quiz />} />
-      </Routes>
-    </BrowserRouter>
+    <Quiz />
   </StrictMode>,
 )
